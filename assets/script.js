0c5e55526162ea136590aff2e6d412ec1a6dd513
ltrapaga <115514660+ltrapaga@users.noreply.github.com>
2022-11-19 21:05:08 -0500

    Update script.js
    Fixed typo

--- assets/script.js
+++ assets/script.js
@@ -36,7 +36,7 @@ var questions = [
     {
         title:
             "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes",
     },
     {
